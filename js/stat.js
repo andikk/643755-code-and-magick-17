9306e70b4145cf623ecc4f2ae27f5eaba400cdbd
andikk <andikk@mail.ru>
2019-06-06 11:16:09 +0300

    Исправил функцию рисования колонки

--- js/stat.js
+++ js/stat.js
@@ -30,27 +30,6 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'top';
-  printText('Ура вы победили!\nСписок результатов:', ctx);
-
-  var maxTime = getMaxElement(times);
-
-  for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), PLAYERS_TEXT_Y);
-    ctx.fillText(Math.round(times[i]), BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), TIMES_TEXT_Y_GAP + (BAR_HEIGHT - Math.round(BAR_HEIGHT * times[i]) / maxTime));
-    ctx.fillStyle = (players[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : getRandomColor();
-
-    drawColumn(ctx, players, times, i, maxTime);
-
-  }
-};
-
 var getRandomColor = function () {
   return 'rgba(0, 0, 255, ' + Math.random() + ')';
 };
@@ -69,6 +48,27 @@ var printText = function (stringToPrint, ctx) {
 
 };
 
-var drawColumn = function (ctx, players, times, i, maxTime) {
+var drawColumn = function (ctx, times, players, i, maxTime) {
+  ctx.fillStyle = '#000';
+  ctx.fillText(players[i], BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), PLAYERS_TEXT_Y);
+  ctx.fillText(Math.round(times[i]), BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), TIMES_TEXT_Y_GAP + (BAR_HEIGHT - Math.round(BAR_HEIGHT * times[i]) / maxTime));
+  ctx.fillStyle = (players[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : getRandomColor();
   ctx.fillRect(BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), COLUMN_Y_GAP + (BAR_HEIGHT - Math.round(BAR_HEIGHT * times[i] / maxTime)), COLUMN_WIDTH, Math.round(BAR_HEIGHT * times[i]) / maxTime);
 };
+
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'top';
+  printText('Ура вы победили!\nСписок результатов:', ctx);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    drawColumn(ctx, times, players, i, maxTime);
+  }
+};
+
