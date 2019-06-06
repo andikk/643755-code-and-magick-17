a5724a1912300e3c25e9f6497c55deba19a113ca
andikk <andikk@mail.ru>
2019-06-06 10:51:26 +0300

    Исправил замечания

--- js/stat.js
+++ js/stat.js
@@ -8,6 +8,10 @@ var GAP = 10;
 var BAR_HEIGHT = 150;
 var COLUMN_WIDTH = 40;
 var COLUMNS_GAP = 50;
+var PLAYERS_TEXT_Y = 250;
+var TIMES_TEXT_Y_GAP = 70;
+var COLUMN_Y_GAP = 100;
+
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -32,20 +36,33 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'top';
-  ctx.fillText('Ура вы победили!', 230, 20);
-  ctx.fillText('Список результатов:', 220, 50);
+  printText('Ура вы победили!\nСписок результатов:', ctx);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(players[i], BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), 250);
-    ctx.fillText(Math.round(times[i]), BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), 70 + (BAR_HEIGHT - Math.round(BAR_HEIGHT * times[i]) / maxTime));
+    ctx.fillText(players[i], BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), PLAYERS_TEXT_Y);
+    ctx.fillText(Math.round(times[i]), BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), TIMES_TEXT_Y_GAP + (BAR_HEIGHT - Math.round(BAR_HEIGHT * times[i]) / maxTime));
     ctx.fillStyle = (players[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : getRandomColor();
-    ctx.fillRect(BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), 100 + (BAR_HEIGHT - Math.round(BAR_HEIGHT * times[i] / maxTime)), COLUMN_WIDTH, Math.round(BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect(BAR_HEIGHT + i * (COLUMN_WIDTH + COLUMNS_GAP), COLUMN_Y_GAP + (BAR_HEIGHT - Math.round(BAR_HEIGHT * times[i] / maxTime)), COLUMN_WIDTH, Math.round(BAR_HEIGHT * times[i]) / maxTime);
   }
 };
 
 var getRandomColor = function () {
   return 'rgba(0, 0, 255, ' + Math.random() + ')';
+};
+
+var printText = function (stringToPrint, ctx) {
+  var START_TEXT_X = 150;
+  var LINE_INDENT = 30;
+  var startTextY = -10;
+
+  var lines = stringToPrint.split('\n');
+
+  for (var i = 0; i < lines.length; i++) {
+    startTextY = startTextY + LINE_INDENT;
+    ctx.fillText(lines[i], START_TEXT_X, startTextY);
+  }
+
 };
